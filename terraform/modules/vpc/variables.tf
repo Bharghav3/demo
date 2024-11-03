c22dcf0abf65825abd14d48e1cc5bf5c5475cd52
Bharghav3 <brock3bharghav@gmail.com>
2024-11-03 21:58:38 +0400

    Removed Modules

--- terraform/modules/vpc/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "private_subnet_cidr_1" {}
-variable "private_subnet_az_1" {}
-variable "private_subnet_cidr_2" {}
-variable "private_subnet_az_2" {}
-variable "vpc_name" {}
-variable "cidr_block" {}
