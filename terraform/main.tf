cad3b44d62b52a4aeb8bb8108b9a36661aa43a9e
Bharghava Raghavan <bharghava.raghavan@almasraf.ae>
2024-11-04 09:11:59 +0400

    Updated Pipeline YAML

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,15 @@ provider "aws" {
   region = "eu-west-1" # Change as needed
 }
 
+terraform {
+  backend "s3" {
+    bucket         = "demo-tfs-state"
+    key            = "terraform/terraform.tfstate" 
+    region         = "eu-west-1"
+    encrypt        = true
+  }
+}
+
 resource "aws_vpc" "eks_vpc" {
   cidr_block = "10.0.0.0/16"
 }
